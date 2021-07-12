de8044ab340849bcb83450b323c09726cdbd1374
Fabio Pontes <faabiopontes@gmail.com>
2021-07-12 20:34:47 -0300

    fix: Added Promise to loadLastRankingRepository method

--- src/data/contracts/load-last-ranking-repository.ts
+++ src/data/contracts/load-last-ranking-repository.ts
@@ -1,5 +1,5 @@
 import { RankingScoreModel } from '@/data/models';
 
 export type LoadLastRankingRepository = {
-  loadLastRanking: () => RankingScoreModel[]
+  loadLastRanking: () => Promise<RankingScoreModel[]>
 }
